28c20fd45d21fbcedecf1da7bcc34709b188ed4c
Yekhlef <f.yekhlef@gmail.com>
2019-07-16 17:22:06 +0200

    toujours sur le form

--- src/main/webapp/WEB-INF/inscription.jsp
+++ src/main/webapp/WEB-INF/inscription.jsp
@@ -13,21 +13,21 @@
 		<form method="post" action="inscription" class="col-6">
            <div class="form-group">
 				<label for="nom">Nom : </label>
-           		<input type="text" class="form-control" placeholder="Entrer nom" name="nom" id="nom"  />
+           		<input type="text" class="form-control" placeholder="Entrer nom" name="nom" id="nom" required />
            		<p id ="nom-helper"></p>
            </div>
            <div class="form-group">
            		<label for="prenom">Prenom : </label>
-           		<input type="text" class="form-control" placeholder="Entrer prénom" name="prenom" id="prenom" />
+           		<input type="text" class="form-control" placeholder="Entrer prénom" name="prenom" id="prenom" required />
            		<p id ="prenom-helper"></p>
            </div>
            <div class="form-group">
            		<label for="age">Age : </label>
-           		<input type="text" class="form-control" placeholder="Entrer âge" name="age" id="age" />
+           		<input type="text" class="form-control" placeholder="Entrer âge" name="age" id="age" required />
            		<p id ="age-helper"></p>
            </div>
            
-           <button type="submit" class="btn btn-primary">Submit</button>
+           <button type="submit" class="btn btn-primary" id= "submit-inscription">Submit</button>
 		</form>
 	
 	</div>
--- src/main/webapp/asset/js/formVerif.js
+++ src/main/webapp/asset/js/formVerif.js
@@ -2,19 +2,59 @@ let inputNomElmt = document.querySelector('#nom');
 let inputPrenomElmt = document.querySelector('#prenom');
 let inputAgeElmt = document.querySelector('#age');
 let nomHelper = document.querySelector('#nom-helper');
+let prenomHelper = document.querySelector('#prenom-helper');
+let buttonSubmitElmt = document.querySelector('#submit-inscription');
+let erreur = 
+
+buttonSubmitElmt.setAttribute('disabled', '');
 
 console.log(inputAgeElmt);
 console.log(inputNomElmt);
 console.log(inputPrenomElmt);
 
+desactiverButton();
+
 inputNomElmt.addEventListener('input', (e)=> {
     if(e.target.value.trim().length > 5) {
     	nomHelper.textContent = 'le nom est trop long';
+    	desactiverButton()
     }else if (e.target.value.trim().length < 2 && e.target.value.trim().length > 0) {
     	nomHelper.textContent = 'le nom est trop court';
+    	desactiverButton();
+    	
 	}else if (e.target.value.trim().length == 0) {
     	nomHelper.textContent = 'le champ est obligatoire';
+    	desactiverButton();
 	}else {
 		nomHelper.textContent = '';
+		activerButton()
 	}
 });
+
+
+inputPrenomElmt.addEventListener('input', (e)=> {
+    if(e.target.value.trim().length > 5) {
+    	prenomHelper.textContent = 'le prenom est trop long';
+    	desactiverButton()
+    }else if (e.target.value.trim().length < 2 && e.target.value.trim().length > 0) {
+    	prenomHelper.textContent = 'le prenom est trop court';
+    	desactiverButton();
+    	
+	}else if (e.target.value.trim().length == 0) {
+		prenomHelper.textContent = 'le champ est obligatoire';
+    	desactiverButton();
+	}else {
+		prenomHelper.textContent = '';
+		activerButton()
+	}
+});
+
+
+
+function desactiverButton() {
+	buttonSubmitElmt.setAttribute('disabled', '');
+}
+
+function activerButton() {
+	buttonSubmitElmt.removeAttribute('disabled');
+}
